01733dca60e13a35f19975c6e0a4ae331e6e2df9
THOKALANANDINI <110174908+THOKALANANDINI@users.noreply.github.com>
2023-01-19 13:34:30 +0530

    1 file modified, 1 added

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,6 +1,26 @@
 import React from 'react';
-
+import './Counter.css';
 function Counter() {
-  return <div>Counter</div>;
+  const [data, setData] = React.useState(0);
+
+  function Incre() {
+    setData((data) => data + 1);
+  }
+  function Decre() {
+    setData(data - 1);
+  }
+  function Set() {
+    setData(0);
+  }
+  return (
+    <div>
+      <h1>Counter:{data}</h1>
+      <button onClick={Incre}>Incre+1</button>
+      <button onClick={Decre}>Decre+1</button>
+      <br />
+      <br />
+      <button onClick={Set}>Reset</button>
+    </div>
+  );
 }
 export default Counter;
